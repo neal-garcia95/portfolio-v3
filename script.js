58aac0becc05bc456cbb7357af1ceed783c68355
neal-garcia95 <neal@builtbymasonry.com>
2023-07-14 15:19:36 -0500

    greensock load and scroll animations for funsies

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// import { gsap } from "gsap";
+
 function show(element){
     element.querySelector("svg").style.display = "block"
 }
@@ -5,3 +7,26 @@ function show(element){
 function hide(element){
     element.querySelector("svg").style.display = "none"
 }
+
+var timeline = new TimelineMax();
+    timeline.from(".hero", 0.3, {y:-100, opacity:0},0.2)
+    timeline.from(".currently", 0.3, {y:100, opacity:0},0.2)
+    timeline.from(".list", 0.3, {y:100, opacity:0},0.2)
+
+gsap.from(".projects", {
+    scrollTrigger: {
+        trigger: ".projects",
+        start: 500
+    },
+    y: 100,
+    opacity: 0,
+})
+
+gsap.from(".more", {
+    scrollTrigger: {
+        trigger: ".more",
+        start: 1200
+    },
+    y: 100,
+    opacity: 0,
+})
